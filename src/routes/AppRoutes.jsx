a6881b8d45475b77c00a42bbf1d2bb4edcd671c1
WebWithAstha <lodhiastha85@gmail.com>
2024-07-10 19:57:59 +0530

    Trailer page created

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,17 +7,20 @@ import People from '../components/People'
 import Movie from '../components/Movie'
 import TvShow from '../components/TvShow'
 import MovieDetails from '../components/partials/MovieDetails'
+import Trailer from '../components/partials/Trailer'
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/trending" element={<Trending />} />
-        <Route path="/popular" element={<Popular />} />
-        <Route path="/movies" element={<Movie />} />
-        <Route path="/people" element={<People />} />
-        <Route path="/tvshows" element={<TvShow />} />
-        <Route path="/:category/details/:id" element={<MovieDetails />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/trending" element={<Trending />} />
+      <Route path="/popular" element={<Popular />} />
+      <Route path="/movies" element={<Movie />} />
+      <Route path="/people" element={<People />} />
+      <Route path="/tvshows" element={<TvShow />} />
+      <Route path="/movie/details/:id" element={<MovieDetails />} >
+        <Route path='trailer/:id' element={<Trailer />} />
+      </Route>
     </Routes>
   )
 }
