e0315f39a5a8c8b0c7d94bcc54c5e094d3298cc4
WebWithAstha <lodhiastha85@gmail.com>
2024-07-10 19:57:03 +0530

    Trailer fetched via API

--- src/store/actions/movieAction.jsx
+++ src/store/actions/movieAction.jsx
@@ -1,24 +1,24 @@
 export { resetMovie } from "../reducers/MovieSlice";
 import axios from "../../utils/axios";
-import { loadMovie, resetMovie } from "../reducers/MovieSlice";
+import { loadMovie } from "../reducers/MovieSlice";
 
 export const asyncLoadMovie = (id)=>(async (dispatch,getState)=>{
-    // console.log("hey")
     const dets = await axios.get(`/movie/${id}`)
     const cast = await axios.get(`/movie/${id}/credits`)
     const extId = await axios.get(`/movie/${id}/external_ids`)
     const similar = await axios.get(`/movie/${id}/similar`)
     const translations = await axios.get(`/movie/${id}/translations`)
     const watchProviders = await axios.get(`/movie/${id}/watch/providers`)
+    const videos = await axios.get(`/movie/${id}/videos`)
     let info = {
         dets:dets.data,
         cast:cast.data.cast,
         extIds:extId.data,
         similar:similar.data.results,
         translations:translations.data.translations.map(t=>t.english_name),
-        watchProviders:watchProviders.data.results.IN
+        watchProviders:watchProviders.data.results.IN,
+        video:videos.data.results.find(v=>v.type==='Trailer')
     }
-    // console.log(dets.data)
     dispatch(loadMovie(info))
 
 })
